9c6fbc7c75e7b4e4b006389f19ec2debf829aa0b
vulture787 <91385513+vulture787@users.noreply.github.com>
2021-10-30 11:32:27 +0530

    Update sort.java

--- sort.java
+++ sort.java
@@ -8,3 +8,25 @@ public class bub {
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
+ private static void bubble(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean t = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                int temp = 0;
+                if (arr[j] < arr[j - 1]) {
+
+                    temp = arr[i];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+
+                    t = true;
+
+                }
+            }
+            if(t==false)
+                break;;
+        }
+
+    }
+}
