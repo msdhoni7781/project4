45ea5e93d6b4e8074a9dad529cdd89c4c370323d
vulture787 <91385513+vulture787@users.noreply.github.com>
2021-10-30 11:35:41 +0530

    Update sort.java

--- sort.java
+++ sort.java
@@ -30,3 +30,22 @@ public class bub {
 
     }
 }
+ for (int j = 1; j < arr.length - i; j++) {
+                int temp = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean t = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                int temp = 0;
+                if (arr[j] < arr[j - 1]) {
+
+                    temp = arr[i];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+
+                    t = true;
+
+                }
+            }
+            if(t==false)
+                break;;
