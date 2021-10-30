4d3c794c6fbfa04d4e7ad1a38d7a8102589c0413
msdhoni7781 <91384562+msdhoni7781@users.noreply.github.com>
2021-10-30 11:30:34 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,10 @@
+package prac;
+
+import java.util.Arrays;
+
+public class bub {
+    public static void main(String[] args) {
+        int arr[] = {2,3,4,5,6};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
